c6b9c70a2673b4faee9563c948f277eb40234659
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2022-05-12 18:36:43 +0300

    added test scene, player and camera controller, cel shading (need to fix)

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [Header("Refs Settings")]
+    [SerializeField] CharacterController controller;
+    [SerializeField] Transform gameMainCamera;
+
+    [Header("Constants")]
+    [SerializeField] float speed = 6;
+    [SerializeField] float gravity = 9.81f;
+    [SerializeField] float inputThreshold = 0.1f;
+    [SerializeField] float jumpHeight = 3;
+    Vector3 velocity;
+
+    [Header("Ground Check")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
+
+    [Header("Smoothness")]
+    [SerializeField] float turnSmoothTime = 0.1f;
+    // [Range(0, 2)]
+    // [SerializeField] float sensitivityX = 0.8f;
+    // [Range(0, 2)]
+    // [SerializeField] float sensitivityY = 1f;
+    //Vector3 sensitivity;
+    float turnSmoothVelocity;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //sensitivity = new Vector3(sensitivityX, 0f, sensitivityY);
+        gravity *= 1f;
+    }
+
+    void Update()
+    {
+        //jump
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0f)
+            velocity.y = -2f;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        //walk
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction =  new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude > inputThreshold)
+        {   
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + gameMainCamera.rotation.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);    
+                
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
